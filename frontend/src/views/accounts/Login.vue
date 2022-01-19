0dcfe9a2dfe5f187a50b586e222a9860ad8d7402
chlaudwo930 <chlaudwo930@naver.com>
2022-01-19 13:57:22 +0900

    MJ

--- frontend/src/views/accounts/Login.vue
+++ frontend/src/views/accounts/Login.vue
@@ -1,26 +1,32 @@
 <template>
-  <div class="login_container">
-    <div class="login">
-      <h1>Login</h1>
-      <div>
-        <label for="email"></label>
-        <input type="text"
-        id="email"
-        v-model="credentials.email"
-        placeholder="email">
+  <div id="background">
+    <div class="container" id="login">
+      <div class="container" id="content">
+        <div>
+          <h4>어서오세요</h4> 
+          <h4>오늘은 어떤 이야기를</h4>
+          <h4>들려주실건가요?</h4>
+        </div>
+        <div>
+          <label for="email"></label>
+          <input type="text"
+          id="email"
+          v-model="credentials.email"
+          placeholder="email">
+        </div>
+        <div>
+          <label for="pw"></label>
+          <input type="password" 
+          id="pw"
+          v-model="credentials.pw"
+          placeholder="passowrd">
+        </div>
+        <div>Don't have an ID?</div>
+        <router-link :to="{ name: 'Signup' }"
+        class="gosignup">Create one now.</router-link>
+        <br>
+        <button @click="login">로그인</button>
       </div>
-      <div>
-        <label for="pw"></label>
-        <input type="password" 
-        id="pw"
-        v-model="credentials.pw"
-        placeholder="passowrd">
-      </div>
-      <div>Don't have an ID?</div>
-      <router-link :to="{ name: 'Signup' }"
-      class="gosignup">Create one now.</router-link>
-      <br>
-      <button @click="login">LOGIN</button>
     </div>
   </div>
 </template>
@@ -57,3 +63,29 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+/* landscape-green-sunlight-sky-atmosphere-universe-1428591-pxhere 1 */
+#background{
+  background-image: url("~@/assets/background.png");
+  /* border: 1px solid #000000; */
+  box-sizing: border-box;
+  min-height: 100vh;
+  background-size: cover;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+#login{
+  border: 2px black;
+  width: 500px;
+  height: 600px;
+  background-color: white;
+  border-radius: 10px;
+}
+/* #content{
+  margin: 50px;
+} */
+/* button{
+
+} */
+</style>
