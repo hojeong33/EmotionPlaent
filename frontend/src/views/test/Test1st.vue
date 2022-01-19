4f14ec97ed914770c9b32dd84e9476c7538d918d
조은누리 <jounuli@naver.com>
2022-01-19 11:12:13 +0900

    background img

---
+++ frontend/src/views/test/Test1st.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="background">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Test1st',
+}
+</script>
+
+<style>
+/* landscape-green-sunlight-sky-atmosphere-universe-1428591-pxhere 1 */
+#background{
+
+  background-image: url("~@/assets/background.png");
+  border: 1px solid #000000;
+  box-sizing: border-box;
+  min-height: 100vh;
+  background-size: cover;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+
+}
+  
+</style>
