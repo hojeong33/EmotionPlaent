b5f181c79fb8877954ba92344a31f155a5235c60
hojeong <jhj20071@naver.com>
2022-01-20 16:03:55 +0900

    HJ navbar base

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,12 +2,20 @@
   <div id="app">
     <nav class="navbar navbar-expand-lg" id="navbar-default">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#"><img src="@/assets/images/EMOTION PLANET.png" id="logo_img" alt=""></a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <div class="row-3">
+          <div class="column-2">
+            <a class="navbar-brand" href="#"><img src="@/assets/images/EMOTION PLANET.png" id="logo_img" alt=""></a>
+          </div>
+        <!-- <router-link :to="{ name: 'Signup' }" class="gosignup">회원가입</router-link> -->
+        </div>
+        <div class="row-3">
+          <form class="d-flex">
+            <input class="form-control" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+          </form>
+        </div>
+        <div class="row-3">
+          <ul class="navbar-nav">
             <li class="nav-item">
               <a class="nav-link active" aria-current="page" href="#">글작성</a>
             </li>
@@ -24,10 +32,6 @@
               <a class="nav-link ">설정</a>
             </li>
           </ul>
-          <form class="d-flex">
-            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success" type="submit">Search</button>
-          </form>
         </div>
       </div>
     </nav>
@@ -62,10 +66,16 @@
   color: #42b983;
 }
 #navbar-default{
-  background-color: rgb(94, 57, 179)
+  background-color: rgb(94, 57, 179);
+  padding-left: 220px;
+  padding-right: 220px;
+
 }
 #logo_img{
   width: 70px;
   height: 40px;
+}
+.container-fuild{
+
 }
 </style>
