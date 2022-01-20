c57f106b1b3967728c808049429e40c6908fbd0b
chlaudwo930 <chlaudwo930@naver.com>
2022-01-20 15:52:10 +0900

    MJ #1. Navbar와 관련 이미지

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,30 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <nav class="navbar navbar-expand-lg" id="navbar-default">
+      <div class="container">
+        <a class="navbar-brand" href="#"><img src="@/assets/images/EMOTION PLANET.png" id="logo_img" alt=""></a>
+        <!-- <router-link :to="{ name: 'Signup' }" class="gosignup">회원가입</router-link> -->
+        <form class="d-flex">
+          <input class="form-control" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit"><img src="@/assets/images/search.png" id="search"></button>
+        </form>
+        <a class="nav-link active" aria-current="page" href="#"><img src="@/assets/images/more.png" id="write"></a>
+        <a class="nav-link" href="#"><img src="@/assets/images/home.png" id="home"></a>
+        <a class="nav-link "><img src="@/assets/images/user.png" id="my_page"></a>
+        <a class="nav-link "><img src="@/assets/images/bell.png" id="alarm"></a>
+        <a class="nav-link "><img src="@/assets/images/settings.png" id="setting"></a>
+      </div>
+    </nav>
+    <!-- <div id="nav">
       <router-link to="/login">Login</router-link> |
       <router-link to="/signup">Signup</router-link> |
       <router-link to="/test1st">Test1st</router-link>
-    </div>
+    </div> -->
     <router-view/>
   </div>
 </template>
 
-<style>
+<style scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -30,4 +45,42 @@
 #nav a.router-link-exact-active {
   color: #42b983;
 }
+#navbar-default{
+  background-color: rgb(94, 57, 179);
+
+}
+#logo_img{
+  width: 70px;
+  height: 40px;
+}
+.container{
+  display:flex;
+  flex-direction: row;
+  justify-content: center;
+  flex-wrap: nowrap;
+}
+#setting{
+  width: 25px;
+  height: 25px;
+}
+#write{
+  width: 25px;
+  height: 25px;
+}
+#home{
+  width: 25px;
+  height: 25px;
+}
+#my_page{
+  width: 25px;
+  height: 25px;
+}
+#alarm{
+  width: 25px;
+  height: 25px;
+}
+#search{
+  width: 25px;
+  height: 25px;
+} 
 </style>
