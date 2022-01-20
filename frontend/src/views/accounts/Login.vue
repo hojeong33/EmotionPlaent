024b9a70f72667a42f8eb281881e697820c0b839
chlaudwo930 <chlaudwo930@naver.com>
2022-01-20 11:00:11 +0900

    MJ #2.로그인 CSS 완성

--- frontend/src/views/accounts/Login.vue
+++ frontend/src/views/accounts/Login.vue
@@ -2,9 +2,12 @@
   <div id="login_container">
     <div id="login">
       <div id="login_header">
-        <h3>어서오세요!</h3>
-        <h3>오늘은 어떤 이야기를</h3>
-        <h3>들려주실건가요?</h3>
+        <div>
+          <h3>어서오세요!</h3>
+          <h3>오늘은 어떤 이야기를</h3>
+          <h3>들려주실건가요?</h3>
+        </div>
+        <img src="../../assets/images/sun.png" id="earth">
       </div>
       <div id="login_body">
         <div id="email">
@@ -12,7 +15,8 @@
           <label for="email"></label>
           <input type="text"
           id="email"
-          v-model="credentials.email">
+          v-model="credentials.email"
+          placeholder=" type your email">
         </div>
         <br>
         <div id="password_label">
@@ -20,26 +24,33 @@
           <label for="pw"></label>
           <input type="password" 
           id="pw"
-          v-model="credentials.pw">
+          v-model="credentials.pw"
+          placeholder=" type your password">
         </div>
       </div>
       <br>
-      <div>
+      <div id="link">
         <a href="#">이메일 찾기 </a> &nbsp;&nbsp;&nbsp;
         <a href="#">비밀번호 찾기 </a> &nbsp;&nbsp;&nbsp;
         <router-link :to="{ name: 'Signup' }" class="gosignup">회원가입</router-link>
       </div>
       <br>
+
       <div>
         <button id="login_button" @click="login">로그인</button>
       </div>
       <br>
-      <div>
-        <button id="google_button">Google로 로그인</button>
-      </div>
+
+      <button id="google_button">
+        <img id="google" src="../../assets/images/Google__G__Logo.png">
+        <p>Google로 로그인</p>
+      </button>
       <br>
       <div>
-        <button id="kakao_button">Kakao로 로그인</button>
+        <button id="kakao_button">
+          <img id="kakao" src="../../assets/images/kakao.png">
+          <p id="kakao_text">Kakao로 로그인 &nbsp;</p>
+          </button>
       </div>
     </div>
   </div>
@@ -82,18 +93,22 @@ export default {
 <style scoped>
 @import '../../assets/styles/astrostyle.css';
 #login_container{
+  display: flex;
+  justify-content: center;
   border: 1px;
   background-color: white;
-  width: 300px;
-  height: 450px;
+  width: 360px;
+  height: 470px;
   border-radius: 10px;
   margin: auto;
 }
 #login{
-  padding: 25px 25px 25px;
+  padding: 20px 10px 20px;
 }
 #login_header{
   padding-bottom:20px;
+  display: flex;
+  justify-content: space-between;
 }
 #email_label{
   text-align: left;
@@ -111,14 +126,14 @@ input{
   border-radius: 5px;
   border-color: #5E39B3;
 }
-a{
-  font-size: 10px;
+#link{
+  font-size: 13px;
 }
 #login_button{
   background-color: #5E39B3;
   border-radius: 20px;
-  width: 100px;
-  height: 30px;
+  width: 120px;
+  height: 40px;
   font-size: 14px;
   font-weight: bold;
   color: white;
@@ -127,25 +142,50 @@ h3{
   text-align: left;
 }
 #google_button{
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
   background-color: #F0EDE7;
   border-radius: 20px;
-  width: 200px;
-  height: 30px;
+  width: 220px;
+  height: 40px;
   font-size: 14px;
   font-weight: bold;
   color: black;
+  margin: auto;
+  padding: 10px;
 }
 #kakao_button{
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
   background-color: #F0EDE7;
   border-radius: 20px;
-  width: 200px;
-  height: 30px;
+  width: 220px;
+  height: 40px;
   font-size: 14px;
   font-weight: bold;
   color: black;
+  margin: auto;
+  padding: 8px;
 }
 img{
-  width: 50px;
-  height: 50px;
+  width: 90px;
+  height: 90px;
+}
+#kakao{
+  width:22px;
+  height: 22px;
+  align-content: center;
+}
+#google{
+  width:18px;
+  height: 18px;
+}
+#earth{
+  margin-top: 10px;
+  margin-bottom: 10px;
+  width: 55px;
+  height: 55px;
 }
 </style>
