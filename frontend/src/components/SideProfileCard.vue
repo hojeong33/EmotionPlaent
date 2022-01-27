5f479e12eac706ffbc7cc1f8bfa73bbf624e783c
조은누리 <jounuli@naver.com>
2022-01-27 22:16:17 +0900

    feature: pick list 붙임

--- frontend/src/components/SideProfileCard.vue
+++ frontend/src/components/SideProfileCard.vue
@@ -37,12 +37,12 @@ export default {
   data() {
     return {
       planetStyles: [
-        { id: 1, name: '공포행성', img: "fear.png", color: '#ED5A8E' },
-        { id: 2, name: '행복행성', img: "happy.png", color: '#6BD9E8' },
+        { id: 1, name: '행복행성', img: "happy.png", color: '#6BD9E8' },
+        { id: 2, name: '우울행성', img: "depressed.png", color: '#2A61F0' },
         { id: 3, name: '중립행성', img: "neutral.png", color: '#C5D3DC' },
-        { id: 4, name: '놀라움행성', img: "surprised.png", color: '#FEA95C' },
-        { id: 5, name: '분노행성', img: "rage.png", color: '#FB5D38' },
-        { id: 6, name: '우울행성', img: "depressed.png", color: '#2A61F0' },
+        { id: 4, name: '공포행성', img: "fear.png", color: '#ED5A8E' },
+        { id: 5, name: '깜짝행성', img: "surprised.png", color: '#FEA95C' },
+        { id: 6, name: '분노행성', img: "rage.png", color: '#FB5D38' },
       ]
     }
   },
--- frontend/src/components/user/PickList.vue
+++ frontend/src/components/user/PickList.vue
@@ -1,13 +1,84 @@
 <template>
-  <div></div>
+  <div id="pick_container">
+    <div id="sort_bar">
+      <p style="font-size:1.8rem; font-weight:bold">우울행성</p>
+      <img id="rocket" src="@/assets/images/icons/rocket.png">
+    </div>
+    <div id="list_tab">
+      <p>음악</p>
+      <p>영화</p>
+      <p>활동</p>
+    </div>
+    <div id="item_container">
+      <span id="item"></span>
+      <span id="item"></span>
+      <span id="item"></span>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-
+  name: 'PickList',
 }
 </script>
 
-<style>
+<style scoped>
+  #pick_container {
+    width: 100%;
+    min-width: 700px;
+    min-height: 100vh;
+    padding-top: 1rem;
+    margin: 2rem auto;
+    display: flex;
+    flex-direction: column;
+    border: 0.1rem solid gainsboro;
+  }
 
+  #sort_bar {
+    display: flex;
+    align-self: flex-end;
+    margin-top: 2rem;
+    margin-right: 1.9rem;
+  }
+
+  #rocket {
+    width: 3vh;
+    height: 3vh;
+    min-width: 10px;
+    margin-top: 0.35rem;
+    margin-left: 0.5rem;
+  }
+
+  #list_tab {
+    display: flex;
+    justify-content: center;
+    width: 40%;
+    margin-left: 13.3rem;
+    margin-top: 2rem;
+    margin-bottom: 2rem;
+    justify-content: space-evenly;
+  }
+
+  #item_container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    width: 100%;
+  }
+
+  #item {
+    width: 40rem;
+    height: 3.5rem;
+    margin: 1.15rem auto;
+    background-color: antiquewhite;
+    border-radius: 20px;
+  }
+
+  p {
+    color: black;
+    font-size: 1.4rem;
+    font-weight: bold;
+    letter-spacing: 0.05rem;
+  }
 </style>
