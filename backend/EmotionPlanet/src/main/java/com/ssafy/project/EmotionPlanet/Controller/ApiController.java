7abd24f8b590034f7a63c69407fe7502d363a855
soonil <soonil0119@naver.com>
2022-02-03 17:40:05 +0900

    feat: API 데이터 가공 완료

--- backend/EmotionPlanet/src/main/java/com/ssafy/project/EmotionPlanet/Controller/ApiController.java
+++ backend/EmotionPlanet/src/main/java/com/ssafy/project/EmotionPlanet/Controller/ApiController.java
@@ -1,51 +1,62 @@
 package com.ssafy.project.EmotionPlanet.Controller;
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
-import com.ssafy.project.EmotionPlanet.Dto.ApiDto;
+import com.ssafy.project.EmotionPlanet.Dto.MovieDto;
+import com.ssafy.project.EmotionPlanet.Dto.MusicDto;
 import com.ssafy.project.EmotionPlanet.Service.ApiServiceImpl;
-import com.ssafy.project.EmotionPlanet.Util.createAccesstoken;
-
-import org.springframework.web.client.RestTemplate;
 
 @CrossOrigin(origins = "http://localhost:5500", allowCredentials = "true", allowedHeaders = "*", methods = {
 		RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE, RequestMethod.HEAD,
 		RequestMethod.OPTIONS })
 
 @RestController
+@RequestMapping("/recommend")
 public class ApiController {
 	
 	@Autowired
 	ApiServiceImpl apiservice;
-	private final RestTemplate restTemplate = new RestTemplate();
-	private final String URL = "https://api.spotify.com/v1/audio-features/{id}";
+	private final int SUCESS = 1;
+	@GetMapping(value ="/movieup")
+	public ResponseEntity<Integer> movieinsert(){ // TMDB에서 인기순으로 500개의 영화를 가져와서 디비에 저장
+		if(apiservice.MovieInsert() == SUCESS)
+			return new ResponseEntity<Integer>(SUCESS, HttpStatus.OK);
+		throw new ResponseStatusException(HttpStatus.FORBIDDEN, "다시 시도해주세요");
+	} 
 	
-	@GetMapping(value = "/api/{id}")
-	public ApiDto AudioFeatures(@PathVariable String id) {
-		createAccesstoken create = new createAccesstoken();
-
-		final HttpHeaders headers = new HttpHeaders();
-		headers.set("Authorization", "Bearer "+create.accesstoken());
-
-
-		final HttpEntity<String> entity = new HttpEntity<String>(headers);
-
-		return restTemplate.exchange(URL, HttpMethod.GET, entity, ApiDto.class, id).getBody();
+	@GetMapping(value = "/movie/{mood}/{type}") // 장르 별로 DB에서 데이터 가져오는거
+	public ResponseEntity<List<MovieDto>> Movie(@PathVariable String mood, @PathVariable String type) {
+		int moodnumber = Integer.parseInt(mood);
+		int typenumber = Integer.parseInt(type);
+		List<MovieDto> list = apiservice.Movie(moodnumber, typenumber);
+		if(list.size()!=0) {
+			return new ResponseEntity<List<MovieDto>>(list, HttpStatus.OK);
+		}else {
+			throw new ResponseStatusException(HttpStatus.FORBIDDEN, "다시 시도해주세요");
+		}
 	}
 	
-	@GetMapping(value = "/api2")
-	public String Recommendations() {
-		return apiservice.Recommendations();
+	@GetMapping(value = "/music/{mood}/{type}") //여기에 감정이랑 뭐 해소 or 심화 감정 데이터 같이 넘겨야 할듯? 갯수 제한도?
+	public ResponseEntity<List<MusicDto>> Music(@PathVariable String mood, @PathVariable String type) {
+		int moodnumber = Integer.parseInt(mood);
+		int typenumber = Integer.parseInt(type);
+		List<MusicDto> list = apiservice.Music(moodnumber,typenumber);
+		if(list.size()!=0) {
+			return new ResponseEntity<List<MusicDto>>(list, HttpStatus.OK);
+		}else {
+			throw new ResponseStatusException(HttpStatus.FORBIDDEN, "다시 시도해주세요");
+		}
 	}
 }
-
-// 11dFghVXANMlKmJXsNCbNl
--- backend/EmotionPlanet/src/main/java/com/ssafy/project/EmotionPlanet/Util/createAccesstoken.java
+++ backend/EmotionPlanet/src/main/java/com/ssafy/project/EmotionPlanet/Util/createAccesstoken.java
@@ -5,13 +5,19 @@ import com.wrapper.spotify.exceptions.SpotifyWebApiException;
 import com.wrapper.spotify.model_objects.credentials.ClientCredentials;
 import com.wrapper.spotify.requests.authorization.client_credentials.ClientCredentialsRequest;
 import org.apache.hc.core5.http.ParseException;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
+@Component
 public class createAccesstoken {
-	private static final String clientId = "1f072a1166fd47dfbe23973e82a573ee";
-	private static final String clientSecret = "a8709cb4a4a249e5b586ac8316cb1bb6";
-	private static SpotifyApi spotifyApi = new SpotifyApi.Builder().setClientId(clientId).setClientSecret(clientSecret)
+	
+	private String clientId = "1f072a1166fd47dfbe23973e82a573ee";
+	private String clientSecret = "a8709cb4a4a249e5b586ac8316cb1bb6";
+	
+	
+
+	private SpotifyApi spotifyApi = new SpotifyApi.Builder().setClientId(clientId).setClientSecret(clientSecret)
 			.build();
 
 	public  String accesstoken() {
@@ -20,7 +26,6 @@ public class createAccesstoken {
 			final ClientCredentials clientCredentials = clientCredentialsRequest.execute();
 			// Set access token for further "spotifyApi" object usage
 			spotifyApi.setAccessToken(clientCredentials.getAccessToken());
-			System.out.println(spotifyApi.getAccessToken());
 			return spotifyApi.getAccessToken();
 		} catch (IOException | SpotifyWebApiException | ParseException e) {
 			System.out.println("Error: " + e.getMessage());
