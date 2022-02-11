7058ae85f92e5216aa8a48b49bc870bf3d3ede83
soonil <soonil0119@naver.com>
2022-02-11 14:59:02 +0900

    feat : 알람 시작

--- frontend/src/views/main/Main.vue
+++ frontend/src/views/main/Main.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
     <side-profile-card :user-info="userInfo"> </side-profile-card>
-    <!-- <button @click="follow">팔로우 신청</button> 알림 테스트부분
+    <!-- <button @click="follow">팔로우 신청</button>
 	<button @click="comment">댓글 알림</button>
 	<button @click="feedlike">피드 좋아요</button>
 	<button @click="picklike">찜 좋아요</button>
-	<button @click="alarmdelete(1)">알림 삭제하기</button>
-	<button @click="alarmselect">알림 가져오기</button> -->
+	<button @click="alarmdelete(1)">알림 삭제하기</button>-->
+	<button @click="alarmselect">알림 가져오기</button> 
     <button @click="socketconnect">소켓 재연결</button>
     <div class="container justify-content-center">
       <div class="example">
@@ -71,7 +71,8 @@ export default {
   },
   created() {
     console.log("유저인포");
-    console.log(session.userInfo);
+    console.log(this.$store.state.userInfo);
+    console.log(session.userInfo)
     if (this.$store.state.recommendReload === 0) {
 
         this.$store.dispatch("recommendMusic");
