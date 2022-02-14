f048b267a68e758ca7d7a2680b29c386883caa4a
soonil <soonil0119@naver.com>
2022-02-14 13:36:17 +0900

    feat: 알림 중복 제거

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -351,7 +351,8 @@ export default new Vuex.Store({
             console.log("알림 날짜 " + obj.date)
             console.log("알림 타입 " + obj.type)
             // alert(obj.message)
-            this.state.alarm.unshift(obj);
+            // this.state.alarm.unshift(obj);
+            this.dispatch('alarmselect')
             console.log("---------------------------------")
           });
         },
