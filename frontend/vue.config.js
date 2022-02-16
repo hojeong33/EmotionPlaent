9bd7afc870bb296353ea58f4064654cb4c64b244
hojeong <jhj20071@naver.com>
2022-02-16 21:19:53 +0900

    마이페이지 작업 전 commit

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -7,8 +7,8 @@ module.exports = {
           //프록시 요청을 보낼 api의 시작 부분
           '/api': {
               // 프록시 요청을 보낼 서버의 주소
-              target: 'https://i6e203.p.ssafy.io:8443',
-              //target: 'http://13.125.47.126:8080',
+            //   target: 'https://i6e203.p.ssafy.io:8443',
+              target: 'http://13.125.47.126:8080',
               changeOrigin: true,
           }
       }
