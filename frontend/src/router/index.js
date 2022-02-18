b7418555552a3fc80618e9caa952554fb7437f88
LKH <mogies@naver.com>
2022-02-18 09:49:40 +0900

    merge

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -329,6 +329,16 @@ router.beforeEach((to, from, next) => {
   next()
 })
 
+const DEFAULT_TITLE = 'Emotion Planet'
+
+router.afterEach((to, from) => {
+  // Use next tick to handle router history correctly
+  // see: https://github.com/vuejs/vue-router/issues/914#issuecomment-384477609
+  Vue.nextTick(() => {
+      document.title = to.meta.title || DEFAULT_TITLE;
+  });
+});
+
 // router.push의 중복 에러 해결방법
 const originalPush = VueRouter.prototype.push;
 VueRouter.prototype.push = function push(location) {
